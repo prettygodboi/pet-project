5ac746bb186ed89cd4feddc99f2d32cb915dc26b
prettygodboi <dima.atzo@gmail.com>
2023-12-06 18:07:48 +0500

    init commit

---
+++ src/test/java/com/abobus/petproject/PetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abobus.petproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
